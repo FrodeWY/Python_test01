609bcf99669dc66dd5cdfa6f47760d1820178562
q841234684 <841234684@qq.com>
2017-12-13 18:00:56 +0800

    's'

--- src/test05/Subprocess.py
+++ src/test05/Subprocess.py
@@ -1,18 +1,21 @@
 import subprocess
 
+
 def subprocess01():
     print('$nslookup www.python.org')
-    r =subprocess.call(['nslookup','www.baidu.com'])
-    print('exit code :',r)
+    r = subprocess.call(['nslookup', 'www.baidu.com'])
+    print('exit code :', r)
+
 
 # 如果子进程还需要输入，则可以通过communicate()方法输入：
 def subprocess02():
     print("$nslookup")
-    p = subprocess.Popen(['nslookup'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE)
-    out,err = p.communicate(b'set qt=mx\npython.org\nexit\n')
+    p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    out, err = p.communicate(b'set qt=mx\npython.org\nexit\n')
     print(out.decode('gbk'))
-    print('exit code :',p.returncode)
+    print('exit code :', p.returncode)
+
 
 if __name__ == '__main__':
     # subprocess01()
